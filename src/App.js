c9f9a9ffd9deecc977f7e6814c6efccf73272158
fjure <45367873+fjure@users.noreply.github.com>
2021-10-15 13:00:33 +0200

    React JSX

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import "./App.css";
 
 // Daumenregel: Wenn eine Variable nichts aus dem Funktionenskörper benötigt (z.B. Parameter), deklariere sie außerhalb
 // Dadurch wird sie nicht bei jedem Funktionsaufruf neu definiert.
-const title = "react";
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}!
+      </h1>
+      <h1>Hello {getTitle("title")}</h1>
+
+      {/* htmlFor spiegelt das for Attribut aus HTML wieder, so wie className statt class und onClick statt onclick*/}
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
