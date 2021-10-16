750042887d0a4bb9a6d0be9d4f3e4ddfe9ca9e23
fjure <45367873+fjure@users.noreply.github.com>
2021-10-16 14:11:28 +0200

    inline handler for removing item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import * as React from "react";
 import "./App.css";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -17,29 +36,20 @@ const useSemiPersistentState = (key, initialState) => {
 const App = () => {
   console.log("App renders");
 
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [stories, setStories] = React.useState(initialStories);
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
   };
 
   const searchedStores = stories.filter((story) =>
@@ -61,7 +71,7 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStores} />
+      <List list={searchedStores} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
@@ -105,30 +115,40 @@ const InputWithLabel = ({
 // Rest operator is used to destructure the objectID from the rest of the item object
 // Afterwards the item object is spread with its key/value pairs into the Item component
 // 1. Step
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
   console.log("List renders");
   return (
     <ul>
       {/* Final Step: Seperate objectID from item object since it is not passed to the item component (Rest-Operator) */}
-      {list.map(({ objectID, ...item }) => (
+      {list.map((item) => (
         // 2. Step: pass all the key-value pairs as attribute/value pairs to the jsx element (Spread-Operator)
-        <Item key={objectID} {...item} />
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
       ))}
     </ul>
   );
 };
 
-const Item = ({ title, url, author, num_comments, points }) => {
+const Item = ({ item, onRemoveItem }) => {
   console.log("Item renders");
+
+  const handleRemoveItem = () => {
+    onRemoveItem(item);
+  };
 
   return (
     <li>
       <span>
-        <a href={url}>{title}</a>
+        <a href={item.url}>{item.title}</a>
       </span>
-      <span>{author}</span>
-      <span>{num_comments}</span>
-      <span>{points}</span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        {/* onClick={() => onRemoveItem(item)} also valid and can/should be used when its a one-liner, more than one-line => extract to new function */}
+        <button type="button" onClick={handleRemoveItem}>
+          Dismiss
+        </button>
+      </span>
     </li>
   );
 };
