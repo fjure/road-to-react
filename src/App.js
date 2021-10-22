eb3e33a368b486a9c1826812864f6acef53c31cb
fjure <45367873+fjure@users.noreply.github.com>
2021-10-22 11:33:27 +0200

    conditional rendering when fetching

--- src/App.js
+++ src/App.js
@@ -20,11 +20,10 @@ const initialStories = [
   },
 ];
 
-const getAsyncStories = () => {
+const getAsyncStories = () =>
   new Promise((resolve) =>
     setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
   );
-};
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -43,11 +42,18 @@ const App = () => {
   console.log("App renders");
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
@@ -83,7 +89,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStores} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStores} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
