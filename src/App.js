7b5173df0660604dea88bfd3787871a5851c8497
fjure <45367873+fjure@users.noreply.github.com>
2021-10-26 11:43:03 +0200

    included axios to fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 import "./App.css";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
@@ -63,18 +64,16 @@ const App = () => {
   const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
-
     dispatchStories({
       type: "STORIES_FETCH_INIT",
     });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => {
